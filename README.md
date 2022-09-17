0b2e054ff147288df73b1749917f2265fafa2b1f
dvher <diego.vergara5@mail.udp.cl>
2022-09-17 20:31:39 -0300

    Added db scripts

---
+++ README.md
@@ -0,0 +1,5 @@
+´´´sql
+create database search_engine;
+create user searchengine@localhost identified by 'S34rch3r_3ng1n3';
+grant all privileges on search_engine.* to searchengine@localhost;
+´´´
---
+++ db_script/db.py
@@ -0,0 +1,15 @@
+from pydal import DAL, Field
+
+db = DAL('mysql://searchengine:S34rch3r_3ng1n3@localhost/search_engine')
+
+db.define_table('data',
+    Field('title', 'string'),
+    Field('description', 'string')
+)
+
+db.define_table('keywords',
+    Field('id_data', 'integer'),
+    Field('keyword', 'string')
+)
+
+db.commit()
